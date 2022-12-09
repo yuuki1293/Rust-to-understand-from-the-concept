0d7344ce614fd03d248c5d088a9c4ed66f589074
yuuki1293 <yuuki1293@outlook.jp>
2022-12-09 23:39:12 +0900

    リスト3.3 C言語でのポインタ型の変数への値の割り当てとそのコピー

--- c/main.c
+++ c/main.c
@@ -1,13 +1,18 @@
+#include <stdio.h>
 int main(){
     int x = 1;
-    int y = x;
-    struct test_t {
-        int x;
-        int y;
-    };
+    int* p = &x;
+    int* q = p;
 
-    struct test_t t = {1, 2};
-    struct test_t s = t;
+    printf("p-1: %d\n", *p); /* p-1: 1 */
+
+    *q = 2;
+
+    /* qとpは同じメモリ領域を指しているのでpが指すメモリ領域に格納されている値も変化 */
+    printf("p-2: %d\n", *p); /* p-2: 2 */
+
+    /* xも変化する */
+    printf("x: %d\n", x); /* x: 2 */
 
     return 0;
 }
