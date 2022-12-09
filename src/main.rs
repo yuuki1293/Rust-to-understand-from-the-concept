b96e4905a0fad0548e6b28929b950d7c603e86e3
yuuki1293 <yuuki1293@outlook.jp>
2022-12-09 23:51:23 +0900

    リスト3.5 関数の引数の変数に所有権が移動する例（コンパイルできない）

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+fn myprint<T: std::fmt::Display>(msg: T) {
+    println!("{}", msg);
+}
+
 fn main() {
     let s = "Hello".to_string();
-    let t = s;
-    println!("{}", t);
-    println!("{}", s);
+    myprint(s); // sの所有権が関数内の変数に移動
+    myprint(s); // sの所有権は移動してしまい初期化されていない変数になるのでエラーになる
 }
