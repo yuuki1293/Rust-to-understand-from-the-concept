a6f5a2249ff38149de7ad406c19fb52a6251c79c
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 00:01:18 +0900

    リスト3.8 ミュータブルなリファレンスは複数作ることができない（コンパイルできない）

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-fn myprint<T: std::fmt::Display>(msg: &T) {
-    println!("{}", *msg);
+fn myclear(x: &mut String){
+    x.clear();
 }
 
 fn main() {
-    let s = "Hello".to_string();
-    myprint(&s); // リファレンスによって関数に渡している
-    myprint(&s); // sが所有権を失わないので2回実行できる
+    let mut s = "Hello".to_string();
+    println!("s= {}", s);
+
+    let s_ref = &mut s;
+    let s_ref2 = &mut s;
+
+    myclear(s_ref);
+    println!("s= {}", s);
 }
