8627bb862c71715f303a1e189516f094fc308789
yuuki1293 <yuuki1293@outlook.jp>
2022-12-09 23:00:37 +0900

    リスト2.2 Rustにおけるテストコード

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,17 @@
-fn func_ex_div_some(x: i32, y: i32) -> Option<i32> {
-    let ans = if y == 0 {
-        None
-    } else {
-        Some(x / y)
-    };
-    ans
+fn add_i32(x: i32, y: i32) -> i32{
+    x + y
 }
 
-fn func_ex_div_result(x: i32, y:i32) -> Result<i32, &'static str> {
-    if y == 0 {
-        Err("div by zero")
-    } else {
-        Ok(x / y)
-    }
+#[test]
+fn test1(){
+    assert_eq!(add_i32(1, 2), 3); //成功
 }
 
-fn func_ex_print_some<T: std::fmt::Display>(ans: Option<T>) {
-    if let Some(x) = ans {
-        println!("{}", x)
-    } else {
-        println!("None")
-    }
-}
-
-fn func_ex_print_some_match<T: std::fmt::Display>(ans: Option<T>) {
-    match ans {
-        Some(x) => println!("{}", x),
-        None => println!("None")
-    }
-}
-
-fn func_ex_print_result<T: std::fmt::Display, E: std::fmt::Display>(ans: Result<T, E>) {
-    match ans {
-        Ok(res) => println!("{}", res),
-        Err(str) => println!("{}", str),
-    }
+#[test]
+fn test2(){
+    assert_eq!(add_i32(2, 4), 7); //失敗
 }
 
 fn main() {
-    func_ex_print_some(func_ex_div_some(10, 5));
-    func_ex_print_some(func_ex_div_some(10, 0));
-    func_ex_print_some_match(func_ex_div_some(10, 5));
-    func_ex_print_some_match(func_ex_div_some(10, 0));
-    func_ex_print_result(func_ex_div_result(10, 5));
-    func_ex_print_result(func_ex_div_result(10, 0));
+    println!("{}", add_i32(2, 5));
 }
