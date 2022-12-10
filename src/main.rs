8495c4f10024016d10f0db655e9f9a5473164e94
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 22:55:45 +0900

    リスト4.9 Option型の値に対してunwrap()を使った例

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-fn func_ex_print_some_match<T: std::fmt::Display>(ans: Option<T>) {
-    match ans {
-        Some(x) => println!("{}", x),
-        None => println!("None"),
+fn func_ex_unwrap(x: i32) -> Option<i32> {
+    if x >= 0 {
+        Some(x)
+    } else {
+        None
     }
+}
+
+fn main() {
+    let rr1 = func_ex_unwrap(5);
+    println!("rr1: {}", rr1.unwrap());
+
+    let rr2 = func_ex_unwrap(-5);
+    println!("rr2: {}", rr2.unwrap());
 }
