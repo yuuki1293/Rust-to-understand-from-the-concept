518e44d64351740c0a793237a55dfa2ce4038aa0
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 23:06:25 +0900

    リスト4.10 is_some()とunwrap()を用いたOption<T>型の処理の例

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
-fn func_ex_unwrap(x: i32) -> Option<i32> {
-    if x >= 0 {
-        Some(x)
+fn func_ex_print_some<T: std::fmt::Display>(ans: Option<T>) {
+    if ans.is_some() {
+        println!("{}", ans.unwrap())
     } else {
-        None
+        println!("None")
     }
-}
-
-fn main() {
-    let rr1 = func_ex_unwrap(5);
-    println!("rr1: {}", rr1.unwrap());
-
-    let rr2 = func_ex_unwrap(-5);
-    println!("rr2: {}", rr2.unwrap());
 }
