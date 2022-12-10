21a62760654b50e23d6d69ac9d1b9e2abc030614
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 14:18:22 +0900

    リスト3.11 引数にリファレンスを受け取り、リファレンスを返却する関数の例

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-fn return_hello() -> &String {
-    let s = "Hello".to_string();
-    &s
+fn pick1(x: &[i32], end: usize) -> &[i32] {
+    &x[..end]
 }
 
 fn main() {
-    let s = return_hello();
-    println!("{}", s);
+    let v1 = [1, 2, 3, 4, 5];
+    let p = pick1(&v1, 2);
+    for ss in p {
+        println!("{}", ss);
+    };
 }
