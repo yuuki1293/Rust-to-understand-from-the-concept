317cbaca683edb5c13820c09704cdcd207e8c88c
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 14:32:07 +0900

    リスト3.12 2つの配列のリファレンスそれぞれから一部を取り出してスライスを返却する例（コンパイルできない）

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-fn pick1(x: &[i32], end: usize) -> &[i32] {
-    &x[..end]
+fn pick2(x: &[i32], y: &[i32], end: usize) -> (&[i32], &[i32]) {
+    (&x[..end], &y[..end])
 }
 
 fn main() {
     let v1 = [1, 2, 3, 4, 5];
-    let p = pick1(&v1, 2);
-    for ss in p {
+    let v2 = [6, 7, 8];
+
+    let p = pick2(&v1, &v2, 2);
+    for ss in p.0 {
         println!("{}", ss);
     };
+    for ss in p.1 {
+        println!("{}", ss);
+    }
 }
