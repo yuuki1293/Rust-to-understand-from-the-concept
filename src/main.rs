56e1466da06d373a255e10ab47d4db3037214cb0
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 15:38:30 +0900

    リスト4.2 リスト4.1に関連関数やメソッドを実装

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 struct Person {
     name: String,
-    age: u8
-};
+    age: u8,
+}
+
+impl Person {
+    // 関連関数
+    fn new(name: String, age: u8) -> Person {
+        Person { name, age }
+    }
+    // メソッド
+    fn age_incr(&self, incr: u8) -> u8 {
+        self.age + incr
+    }
+    // メソッド
+    fn age_incr_replace(&mut self, incr: u8) {
+        self.age += incr;
+    }
+}
