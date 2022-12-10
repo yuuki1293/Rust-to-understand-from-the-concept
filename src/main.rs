be45e8bfd3933675a4eaf54476b917f3f9018645
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 14:12:56 +0900

    リスト3.10 関数内のローカル変数へのリファレンスを返却値にすることはできない（コンパイルできない）

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
-fn myclear(x: &mut String){
-    x.clear();
+fn return_hello() -> &String {
+    let s = "Hello".to_string();
+    &s
 }
 
 fn main() {
-    let mut s = "Hello".to_string();
-    println!("s= {}", s);
-
-    let s_ref = &mut s;
-    myclear(s_ref);
-    println!("s= {}", s);
-
-    let s_ref2 = &mut s;
-    myclear(s_ref2);
-    println!("s= {}", s);
+    let s = return_hello();
+    println!("{}", s);
 }
