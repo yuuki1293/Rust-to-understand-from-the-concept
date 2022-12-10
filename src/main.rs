7022eacb3756d3fcdc0bac06624dfb8b563f7eb9
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 15:33:31 +0900

    リスト4.1 Person型の定義

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,4 @@
-fn incr_by_owned(x: i32) -> i32 {
-    x + 1
-}
-
-fn incr_by_ref(x_ref: &i32) -> i32 {
-    *x_ref + 1
-}
-
-fn main() {
-    let x = 1;
-    let x_ref = &x;
-    println!("{}", incr_by_owned(x));
-    println!("{}", incr_by_ref(x_ref));
-}
+struct Person {
+    name: String,
+    age: u8
+};
