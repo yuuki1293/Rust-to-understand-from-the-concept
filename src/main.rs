dccb2839882a553624bd3fd0f56ba239ae3fc6aa
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 21:31:17 +0900

    リスト4.6 リスト4.5の列挙型の値を作成してlet ifやmatchによって列挙型の値の場合分けをする例

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,34 @@ enum EnumExample {
     TupleTypeExample2(i32, bool),
     StructTypeExample { name: String, age: u8 },
 }
+
+fn main() {
+    let mut v: Vec<EnumExample> = Vec::new();
+
+    v.push(EnumExample::TupleTypeExample1(String::from("Hello")));
+    v.push(EnumExample::TupleTypeExample2(10, true));
+    v.push(EnumExample::StructTypeExample {
+        name: String::from("taro"),
+        age: 10,
+    });
+
+    for e in &v {
+        if let EnumExample::StructTypeExample { name: n, age: a } = e {
+            println!("StructTypeExample_iflet: name = {}, age = {}", n, a);
+        }
+    }
+
+    for e in &v {
+        match e {
+            EnumExample::TupleTypeExample1(s) => {
+                println!("TupleTypeExample1: s = {}", s);
+            }
+            EnumExample::TupleTypeExample2(n, b) => {
+                println!("TupleTypeExample2: n = {}, b = {}", n, b);
+            }
+            EnumExample::StructTypeExample { name: n, .. } => {
+                println!("StructTypeExample: name = {}", n);
+            }
+        }
+    }
+}
