0add834e67e064ca9115412ad9a2e3dfdd86e7bc
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 21:17:39 +0900

    リスト4.4 列挙型を返却する関数の例

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,15 @@
-struct Person {
-    name: String,
-    age: u8,
+enum Sign {
+    Positive,
+    Zero,
+    Negative,
 }
 
-impl Person {
-    // 関連関数
-    fn new(name: String, age: u8) -> Person {
-        Person { name, age }
+fn determine_sign(x: i32) -> Sign {
+    if x > 0 {
+        Sign::Positive
+    } else if x < 0 {
+        Sign::Negative
+    } else {
+        Sign::Zero
     }
-    // メソッド
-    fn age_incr(&self, incr: u8) -> u8 {
-        self.age + incr
-    }
-    // メソッド
-    fn age_incr_replace(&mut self, incr: u8) {
-        self.age += incr;
-    }
-}
-
-fn main() {
-    let mut taro = Person::new(String::from("taro"), 10);
-
-    let age_plus1 = taro.age_incr(1);
-    println!("{}", age_plus1);
-
-    taro.age_incr_replace(10);
-    println!("{}", taro.age);
 }
