b725f2e1d9ee7ce003c5ef84cd7c6529e6fdc043
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 22:48:02 +0900

    リスト4.7 if letを用いたOption<T>型の処理

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,7 @@
-enum EnumExample {
-    TupleTypeExample1(String),
-    TupleTypeExample2(i32, bool),
-    StructTypeExample { name: String, age: u8 },
-}
-
-fn main() {
-    let mut v: Vec<EnumExample> = Vec::new();
-
-    v.push(EnumExample::TupleTypeExample1(String::from("Hello")));
-    v.push(EnumExample::TupleTypeExample2(10, true));
-    v.push(EnumExample::StructTypeExample {
-        name: String::from("taro"),
-        age: 10,
-    });
-
-    for e in &v {
-        if let EnumExample::StructTypeExample { name: n, age: a } = e {
-            println!("StructTypeExample_iflet: name = {}, age = {}", n, a);
-        }
-    }
-
-    for e in &v {
-        match e {
-            EnumExample::TupleTypeExample1(s) => {
-                println!("TupleTypeExample1: s = {}", s);
-            }
-            EnumExample::TupleTypeExample2(n, b) => {
-                println!("TupleTypeExample2: n = {}, b = {}", n, b);
-            }
-            EnumExample::StructTypeExample { name: n, .. } => {
-                println!("StructTypeExample: name = {}", n);
-            }
-        }
+fn func_ex_print_some<T: std::fmt::Display>(ans: Option<T>) {
+    if let Some(x) = ans {
+        println!("{}", x)
+    } else {
+        println!("None")
     }
 }
