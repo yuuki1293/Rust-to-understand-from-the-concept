f02f3b1ea4a1c88443fb1abe5843dbfa788a52a8
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 23:39:13 +0900

    リスト4.14 エラーの種類が複数ある場合のResult<T,E>型の使用例

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
-fn func_ex_print_some<T: std::fmt::Display>(ans: Option<T>) {
-    if ans.is_some() {
-        println!("{}", ans.unwrap())
+enum DivError {
+    DivByZero(i32),         // 0で割り算。i32は分子
+    BothNegative(i32, i32), // 分子、分母ともに負の数。2つのi32はそれぞれ分子、分母
+}
+
+fn mydiv(x: i32, y: i32) -> Result<i32, DivError> {
+    if y == 0 {
+        Err(DivError::DivByZero(x))
+    } else if x < 0 && y < 0 {
+        Err(DivError::BothNegative(x, y))
     } else {
-        println!("None")
+        Ok(x / y)
     }
+}
+
+fn print_mydiv(x: i32, y: i32) {
+    match mydiv(x, y) {
+        Ok(ans) => println!("no error. ans = {}", ans),
+        Err(DivError::DivByZero(a)) => {
+            println!("{} divided by zero", a)
+        }
+        Err(DivError::BothNegative(a, b)) => {
+            println!("Both numerator {} and denominator {} are negative", a, b) // numerator: 分子、denominator: 分母
+        }
+    }
+}
+
+fn main(){
+    print_mydiv(5, 2);
+    print_mydiv(-5, 0);
+    print_mydiv(-5, -2);
 }
