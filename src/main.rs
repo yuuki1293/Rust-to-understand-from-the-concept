8c893507bcbc91e5eef213acf03e2dbdce7083a9
yuuki1293 <yuuki1293@outlook.jp>
2022-12-10 21:24:18 +0900

    リスト4.5 列挙子にデータを持たせた例

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-enum Sign {
-    Positive,
-    Zero,
-    Negative,
-}
-
-fn determine_sign(x: i32) -> Sign {
-    if x > 0 {
-        Sign::Positive
-    } else if x < 0 {
-        Sign::Negative
-    } else {
-        Sign::Zero
-    }
+enum EnumExample {
+    TupleTypeExample1(String),
+    TupleTypeExample2(i32, bool),
+    StructTypeExample { name: String, age: u8 },
 }
