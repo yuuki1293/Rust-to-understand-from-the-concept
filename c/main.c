6b0f1a90c496038776776cadbb0c68ad9e6ed02a
yuuki1293 <yuuki1293@outlook.jp>
2022-12-11 12:09:11 +0900

    リスト4.18 関数内の共通の値を2つの構造体にセットするC言語のコード（コンパイルはできるが正しく動作しない）

--- c/main.c
+++ c/main.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 
-#define BUFSIZE 1024
+struct DataA
+{
+    int *number_a;
+};
+
+struct DataB
+{
+    int *number_b;
+};
+
+void setdata(struct DataA *data_a, struct DataB *data_b, int value)
+{
+    int number = value + 10;
+    data_a->number_a = &number;
+    data_b->number_b = &number;
+}
 
 int main()
 {
-    FILE *fp;
-    char buf[BUFSIZE];
-    int size_read;
-    int ret_val;
+    struct DataA data_a_1;
+    struct DataB data_b_1;
+    struct DataA data_a_2;
+    struct DataB data_b_2;
 
-    if ((fp = fopen("test.txt", "r")) != NULL)
-    {
-        size_read = fread(buf, 1, BUFSIZE, fp);
-        fclose(fp);
-        printf("read size: %d\n", size_read);
-        ret_val = 0;
-    }
-    else
-    {
-        fprintf(stderr, "Failed to open the file");
-        ret_val = 1;
-    }
-    return ret_val;
+    setdata(&data_a_1, &data_b_1, 1);
+    setdata(&data_a_2, &data_b_2, 2);
+    printf("to be 11, 11: %d, %d\n",
+           *data_a_1.number_a,
+           *data_b_1.number_b);
+    printf("to be 12, 12: %d, %d\n",
+           *data_a_2.number_a,
+           *data_b_2.number_b);
+    return 0;
 }
--- py/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-try:
-    with open("test.txt") as fp:
-        buf = fp.read()
-        print("read size: {}".format(len(buf)))
-except Exception:
-    print("Failed to open or read the file", file=sys.stderr)
-    sys.exit(1)
