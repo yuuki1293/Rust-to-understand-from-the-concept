7413737081a55a162f5a76aae9a30549837f8a56
yuuki1293 <yuuki1293@outlook.jp>
2022-12-11 13:02:37 +0900

    リスト4.24 ノードと子ノードの関係付け子ノードをたどるコード（2）

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
+use std::cell::RefCell;
 use std::rc::Rc;
 
 struct Node {
     data: i32,
-    child: Option<Rc<Node>>,
+    child: Option<Rc<RefCell<Node>>>,
 }
 
-fn print_link(start_node: Rc<Node>) {
+fn print_link(start_node: Rc<RefCell<Node>>) {
     let mut p = start_node;
     loop {
-        println!("{}", p.data);
-        if p.child.is_none() {
+        println!("{}", p.borrow().data);
+        if p.borrow().child.is_none() {
             break;
         }
-        p = Rc::clone(p.child.as_ref().unwrap());
+        let ptmp = Rc::clone(p.borrow().child.as_ref().unwrap());
+        p = ptmp;
     }
 }
 
 fn main() {
-    let node3 = Rc::new(Node {
+    let node3 = Rc::new(RefCell::new(Node {
         data: 3,
         child: None,
-    });
+    }));
 
-    let node1 = Rc::new(Node {
+    let node1 = Rc::new(RefCell::new(Node {
         data: 1,
         child: Some(Rc::clone(&node3)),
-    });
-    let node2 = Rc::new(Node {
+    }));
+    let node2 = Rc::new(RefCell::new(Node {
         data: 2,
         child: Some(Rc::clone(&node3)),
-    });
+    }));
+
+    node1.borrow_mut().child = Some(Rc::clone(&node3));
+    node2.borrow_mut().child = Some(Rc::clone(&node3));
 
     println!("link from node1");
     print_link(Rc::clone(&node1));
