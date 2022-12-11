a358aef83ddbe4c07bba9706c7a8ae4479a0e923
yuuki1293 <yuuki1293@outlook.jp>
2022-12-11 12:25:04 +0900

    リスト4.19 関数内の共通の値を2つの構造体にセットするRustのコード（コンパイルできない）

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+struct DataA<'a> {
+    number_a: Option<&'a i32>,
+}
+
+struct DataB<'a> {
+    number_b: Option<&'a i32>,
+}
+
+fn setdata(data_a: &mut DataA, data_b: &mut DataB, value: i32) {
+    let number = Box::new(value * 10);
+    data_a.number_a = Some(&number);
+    data_b.number_b = Some(&number);
+}
+
+fn main() {
+    let mut data_a_1 = DataA { number_a: None };
+    let mut data_b_1 = DataB { number_b: None };
+    let mut data_a_2 = DataA { number_a: None };
+    let mut data_b_2 = DataB { number_b: None };
+
+    setdata(&mut data_a_1, &mut data_b_1, 1);
+    setdata(&mut data_a_2, &mut data_b_2, 2);
+    println!(
+        "to be 11, 11: {}, {}",
+        data_a_1.number_a.unwrap(),
+        data_b_1.number_b.unwrap()
+    );
+    println!(
+        "to b3 12, 12: {}, {}",
+        data_a_2.number_a.unwrap(),
+        data_b_2.number_b.unwrap()
+    );
+}
