0c793b090b0a951047cff7624c20ce6655170684
yuuki1293 <yuuki1293@outlook.jp>
2022-12-11 12:45:17 +0900

    リスト4.22 関数内の共通の値を2つの構造体にセットするRustのコード（正しく動作する）

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 use std::rc::Rc;
 
-struct TestA {
-    data_i32: i32,
-    data_string: String,
+struct DataA {
+    number_a: Option<Rc<i32>>,
+}
+
+struct DataB {
+    number_b: Option<Rc<i32>>,
+}
+
+fn setdata(data_a: &mut DataA, data_b: &mut DataB, value: i32) {
+    let number = Rc::new(value * 10);
+    data_a.number_a = Some(Rc::clone(&number));
+    data_b.number_b = Some(Rc::clone(&number));
 }
 
 fn main() {
-    let x = Rc::new(TestA {
-        data_i32: 1,
-        data_string: String::from("Hello"),
-    });
-    let data_i32 = x.data_i32;
-    let data_string = x.data_string;
-    println!("{}, {}", data_i32, data_string);
+    let mut data_a_1 = DataA { number_a: None };
+    let mut data_b_1 = DataB { number_b: None };
+    let mut data_a_2 = DataA { number_a: None };
+    let mut data_b_2 = DataB { number_b: None };
+
+    setdata(&mut data_a_1, &mut data_b_1, 1);
+    setdata(&mut data_a_2, &mut data_b_2, 2);
+    println!(
+        "to be 11, 11: {}, {}",
+        data_a_1.number_a.unwrap(),
+        data_b_1.number_b.unwrap()
+    );
+    println!(
+        "to b3 12, 12: {}, {}",
+        data_a_2.number_a.unwrap(),
+        data_b_2.number_b.unwrap()
+    );
 }
