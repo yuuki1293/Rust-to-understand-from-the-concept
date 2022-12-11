c1b2fca719a0d98648b35bf34e30ff0f084d2c1e
yuuki1293 <yuuki1293@outlook.jp>
2022-12-11 13:13:26 +0900

    リスト4.25 Rc<T>型の循環参照によってメモリリークが発生するコード

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,24 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-struct Node {
+#[allow(dead_code)]
+struct Node{
     data: i32,
     child: Option<Rc<RefCell<Node>>>,
 }
 
-fn print_link(start_node: Rc<RefCell<Node>>) {
-    let mut p = start_node;
+fn main(){
     loop {
-        println!("{}", p.borrow().data);
-        if p.borrow().child.is_none() {
-            break;
-        }
-        let ptmp = Rc::clone(p.borrow().child.as_ref().unwrap());
-        p = ptmp;
+        let node1 = Rc::new(RefCell::new(Node {
+            data: 1,
+            child: None,
+        }));
+        let node2 = Rc::new(RefCell::new(Node {
+            data: 2,
+            child: None,
+        }));
+
+        node1.borrow_mut().child = Some(Rc::clone(&node2));
+        node2.borrow_mut().child = Some(Rc::clone(&node1));
     }
-}
-
-fn main() {
-    let node3 = Rc::new(RefCell::new(Node {
-        data: 3,
-        child: None,
-    }));
-
-    let node1 = Rc::new(RefCell::new(Node {
-        data: 1,
-        child: Some(Rc::clone(&node3)),
-    }));
-    let node2 = Rc::new(RefCell::new(Node {
-        data: 2,
-        child: Some(Rc::clone(&node3)),
-    }));
-
-    node1.borrow_mut().child = Some(Rc::clone(&node3));
-    node2.borrow_mut().child = Some(Rc::clone(&node3));
-
-    println!("link from node1");
-    print_link(Rc::clone(&node1));
-
-    println!("link from node2");
-    print_link(Rc::clone(&node2));
 }
