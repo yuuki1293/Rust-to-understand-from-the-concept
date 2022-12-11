b9c376118bc25c21883c6720645e5c2c737b53dc
yuuki1293 <yuuki1293@outlook.jp>
2022-12-11 12:32:49 +0900

    リスト4.20 関数内の共通の値を2つの構造体にセットするC言語のコード（正しく動作する）

--- c/main.c
+++ c/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct DataA
 {
@@ -12,9 +13,10 @@ struct DataB
 
 void setdata(struct DataA *data_a, struct DataB *data_b, int value)
 {
-    int number = value + 10;
-    data_a->number_a = &number;
-    data_b->number_b = &number;
+    int* number_ptr = (int *)malloc(sizeof(int));
+    *number_ptr=value+10;
+    data_a->number_a = number_ptr;
+    data_b->number_b = number_ptr;
 }
 
 int main()
@@ -32,5 +34,7 @@ int main()
     printf("to be 12, 12: %d, %d\n",
            *data_a_2.number_a,
            *data_b_2.number_b);
+    free(data_a_1.number_a);
+    free(data_a_2.number_a);
     return 0;
 }
