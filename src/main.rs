c6a4ce35dd29fc5e7fe680493b2f618996f888bf
yuuki1293 <yuuki1293@outlook.jp>
2022-12-23 18:12:45 +0900

    リスト5.5 トレイとの定義例

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+trait CalcArea {
+    fn calc_area(&self) -> f64;
+}
+
 struct Rectangle {
     width: f64,
-    height: f64
+    height: f64,
 }
 
 impl Rectangle {
@@ -11,20 +15,23 @@ impl Rectangle {
 
 struct RightTriangle {
     width: f64,
-    height: f64
+    height: f64,
 }
 
 impl RightTriangle {
-    fn calc_area(&self) -> f64{
+    fn calc_area(&self) -> f64 {
         self.width * self.height * 0.5
     }
 }
 
-fn area<T>(x: &T)->f64{
+fn area<T>(x: &T) -> f64 {
     x.calc_area()
 }
 
 fn main() {
-    let rect = Rectangle{width: 1.0, height: 2.0};
+    let rect = Rectangle {
+        width: 1.0,
+        height: 2.0,
+    };
     println!("{}", area(&rect));
 }
