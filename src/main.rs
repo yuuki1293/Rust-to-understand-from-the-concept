6a58602c33913e73b3e85f81ef857087124e5db5
yuuki1293 <yuuki1293@outlook.jp>
2023-01-20 11:26:19 +0900

    リスト5.14 引数の型によって型パラメータが決まる

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,8 @@
-trait MyError: std::fmt::Debug {}
-
-#[derive(Debug)]
-struct MyError1;
-impl MyError for MyError1 {}
-
-#[derive(Debug)]
-struct MyError2;
-impl MyError for MyError2 {}
-
-#[derive(Debug)]
-struct MyError3;
-impl MyError for MyError3 {}
-
-#[derive(Debug)]
-struct MyErrorOther;
-impl MyError for MyErrorOther {}
-
-fn div4(x: i32) -> Result<(), Box<dyn MyError>> {
-    let res = x % 4;
-    match res {
-        0 => Ok(()),
-        1 => Err(Box::new(MyError1 {})),
-        2 => Err(Box::new(MyError2 {})),
-        3 => Err(Box::new(MyError3 {})),
-        _ => Err(Box::new(MyErrorOther {})),
-    }
+fn generic_return_type<T: std::fmt::Display>(x: T) -> T {
+    x
 }
 
 fn main() {
-    println!("{:?}", div4(0));
-    println!("{:?}", div4(1));
-    println!("{:?}", div4(2));
-    println!("{:?}", div4(3));
+    println!("{}", generic_return_type(1));
+    println!("{}", generic_return_type("Hello"));
 }
