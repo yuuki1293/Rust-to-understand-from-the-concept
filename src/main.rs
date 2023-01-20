08bdc4e45fcde752e0ad1330ea80b5c56611d5e1
yuuki1293 <yuuki1293@outlook.jp>
2023-01-20 13:02:42 +0900

    リスト5.16 Derefトレイトの利用例

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
-use std::fmt::Display;
+use std::ops::Deref;
 
-impl<T: ?Sized + Display> Display for &T {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-        Display::fmt(&**self, f)
+struct MyData {
+    x: i32,
+}
+
+impl Deref for MyData {
+    type Target = i32;
+    fn deref(&self) -> &Self::Target {
+        &self.x
     }
+}
+
+fn deref_data(x: &i32) -> i32 {
+    *x
+}
+
+fn main() {
+    let data = MyData { x: 1 };
+    let data_deref1 = data.deref();
+    let data_deref2 = *(data.deref());
+    let data_deref3 = *data;
+    let data_deref4 = deref_data(&data);
+
+    println!("{}", data_deref1);
+    println!("{}", data_deref2);
+    println!("{}", data_deref3);
+    println!("{}", data_deref4);
 }
