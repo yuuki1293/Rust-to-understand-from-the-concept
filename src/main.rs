1587832f33d1c7a68af40e078f7ba6754925c856
yuuki1293 <yuuki1293@outlook.jp>
2023-01-24 09:08:18 +0900

    リスト6.1 ファイルの内容を固定調のバッファに繰り返し読み出して改行コードで区切った結果をベクター型に詰めるコード

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
-use std::ops::Deref;
+use std::fs::File;
+use std::io::Read;
 
-struct MyData {
-    x: i32,
-}
+const BUFSIZE: usize = 1024;
 
-impl Deref for MyData {
-    type Target = i32;
-    fn deref(&self) -> &Self::Target {
-        &self.x
+fn main() -> std::io::Result<()> {
+    let mut f = File::open("input.txt")?;
+    let mut buf = [0_u8; BUFSIZE];
+
+    let mut lines = Vec::new();
+    let mut linebuf = String::new();
+
+    loop {
+        let read_size = f.read(&mut buf)?;
+
+        if read_size == 0 {
+            break;
+        }
+        for cc in &buf[..read_size] {
+            if *cc == b'\n' {
+                lines.push(linebuf);
+                linebuf = String::new();
+            } else {
+                linebuf.push(*cc as char);
+            }
+        }
     }
-}
+    println!("{:?}", lines);
 
-fn deref_data(x: &i32) -> i32 {
-    *x
-}
-
-fn main() {
-    let data = MyData { x: 1 };
-    let data_deref1 = data.deref();
-    let data_deref2 = *(data.deref());
-    let data_deref3 = *data;
-    let data_deref4 = deref_data(&data);
-
-    println!("{}", data_deref1);
-    println!("{}", data_deref2);
-    println!("{}", data_deref3);
-    println!("{}", data_deref4);
+    Ok(())
 }
