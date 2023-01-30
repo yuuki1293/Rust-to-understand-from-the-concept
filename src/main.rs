76864d80088aef81c8050a8881275bcf96cf71c2
yuuki1293 <yuuki1293@outlook.jp>
2023-01-30 13:32:16 +0900

    リスト7.4 collectの使用例

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-{
-    let result = match IntoIterator::into_iter(values) {
-        mut iter => loop{
-            let next;
-            match iter.next() {
-                Some(val) => next = val,
-                None => break,
-            };
-            let x = next;
-            let () = { println!("{}", x); };
-        },
-    };
-    result
+fn main() {
+    let c = "あいうえお";
+    let c_vec = c.chars().collect::<Vec<char>>();
+    println!("{:?}", c_vec);
 }
