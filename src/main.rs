d2a127f4a7b942cae67875ea46a4ec1bdae8e1a6
yuuki1293 <yuuki1293@outlook.jp>
2023-01-30 13:28:51 +0900

    リスト7.3 forループの実体

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-fn main() {
-    let vv = vec![1, 2, 3, 4];
-    let mut iter = vv.into_iter();
-
-    let x = iter.next().unwrap();
-    println!("{}", x);
-
-    let x = iter.next().unwrap();
-    println!("{}", x);
-
-    let x = vv[2];
-    println!("{}", x);
+{
+    let result = match IntoIterator::into_iter(values) {
+        mut iter => loop{
+            let next;
+            match iter.next() {
+                Some(val) => next = val,
+                None => break,
+            };
+            let x = next;
+            let () = { println!("{}", x); };
+        },
+    };
+    result
 }
