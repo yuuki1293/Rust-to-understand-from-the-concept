4a3df90315b26ed4ce1de39d1d05ecab5e0032a1
yuuki1293 <yuuki1293@outlook.jp>
2023-02-14 10:34:11 +0900

    リスト8.2 ベクターの要素の和をスレッド並列で計算するコード例

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
-use std::thread::spawn;
+const N_MAX: usize = 100000000;
+const N_THREAD: usize = 4;
 
-fn main() {
-    let mut v_threads = Vec::new();
-    for i in 0..10 {
-        let thread = spawn(move || println!("{}", i));
-        v_threads.push(thread);
+const N_ELEM_PER_THREAD: usize = N_MAX / N_THREAD;
+const RESIDUAL: usize = N_MAX % N_THREAD;
+
+fn main() -> std::thread::Result<()> {
+    if RESIDUAL != 0 {
+        panic!("invalid combination of N_MAX and N_THREAD");
     }
-    let _x: Vec<()> = v_threads.into_iter().map(|th| th.join().unwrap()).collect();
+
+    let mut thrd = Vec::new();
+    let v = (1..=N_MAX).collect::<Vec<usize>>();
+
+    // 1..=N_MAXをN_THREADに分割して、それぞれの和をスレッドで計算
+    for ii in 0..N_THREAD {
+        let ist = ii * N_ELEM_PER_THREAD;
+        let ien = ist + N_ELEM_PER_THREAD;
+        let vv = (&v[ist..ien]).to_owned();
+        let th = std::thread::spawn(move || vv.into_iter().sum::<usize>());
+        thrd.push(th);
+    }
+
+    // 各スレッドで計算した値を集めて、その和を取り、全体の和を求める
+    let ans: usize = thrd.into_iter().map(|r| r.join().unwrap()).sum::<usize>();
+    println!("{}", ans);
+    assert_eq!(ans, N_MAX * (N_MAX + 1) / 2);
+    Ok(())
 }
