f37f8ca3a1ab7b2cf22e585b97d2a6f9be4b1552
yuuki1293 <yuuki1293@outlook.jp>
2023-02-14 11:27:38 +0900

    リスト8.10 非同期関数を用いたエコーサーバー

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,51 @@
-async fn sum_func(n: usize) -> usize {
-    let ans = (1..=n).into_iter().sum::<usize>();
-    println!("{}", ans);
-    ans
+use std::io;
+use std::error::Error;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream};
+
+async fn echo_process(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
+    let mut buf = Vec::with_capacity(1024);
+    let mut input_prompt = true;
+    loop {
+        if input_prompt{
+            stream.write("input => ".as_bytes()).await?;
+        }
+        let read = stream.try_read_buf(&mut buf);
+        match read {
+            Ok(0) => break,
+            Ok(n) => {
+                stream.write("output => ".as_bytes()).await?;
+                stream.write_all(&buf[..n]).await?;
+                input_prompt = true;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock=>{
+                input_prompt =false;
+                continue;
+            }
+            Err(e)=>{
+                return Err(e.into())
+            }
+        }
+        buf.clear();
+    }
+    Ok(())
 }
 
-fn main() {
-    let ls = tokio::task::LocalSet::new();
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap();
-    ls.block_on(&rt, async {
-        sum_func(10000000).await;
-        sum_func(20000000).await;
-    });
+#[tokio::main]
+async fn main(){
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2{
+        panic!("port is not specified");
+    }
+    let port: usize = args[1].parse().expect("Failed to get the port number");
+    let addr = format!("localhost:{}", port);
+    let listner = TcpListener::bind(addr).await.unwrap();
+    println!("Listening to the port {}", port);
+
+    loop {
+        let (mut stream, _) = listner.accept().await.unwrap();
+        tokio::spawn(async move{
+            echo_process(&mut stream).await.unwrap();
+        });
+    }
 }
