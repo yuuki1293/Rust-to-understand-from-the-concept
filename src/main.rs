214f19a629e1a7f3eeaef06b2d4ae2cf60fe7008
yuuki1293 <yuuki1293@outlook.jp>
2023-02-14 10:59:14 +0900

    リスト8.5 スレッドを使ったエコーサーバー

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
-use std::sync::Arc;
-use std::thread::spawn;
+use std::error::Error;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn echo_process(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
+    let mut buf = [0_u8; 1024];
+    loop {
+        stream.write("input => ".as_bytes())?;
+        let read = stream.read(&mut buf);
+        match read {
+            Ok(0) => break,
+            Ok(n) => {
+                stream.write("output => ".as_bytes())?;
+                stream.write_all(&buf[..n])?;
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+        }
+    }
+    Ok(())
+}
 
 fn main() {
-    let data = Arc::new(Vec::new());
-
-    let added = vec![1, 2, 3, 4, 5];
-
-    let mut thrd = Arc::new(Vec::new());
-    for aa in added {
-        let data = Arc::clone(&data);
-        let th = spawn(move || {
-            data.push(aa);
-        });
-        thrd.push(th);
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        panic!("port is not specified");
     }
+    let port: usize = args[1].parse().expect("Failed to get the port number");
+    let addr = format!("localhost:{}", port);
+    let listner = TcpListener::bind(addr).unwrap();
+    println!("Listening to the port {}", port);
 
-    thrd.into_iter().for_each(|th| {
-        let _ = th.join();
-    });
-
-    println!("{:?}", *data);
+    loop {
+        let (mut stream, _) = listner.accept().unwrap();
+        std::thread::spawn(move || {
+            echo_process(&mut stream).unwrap();
+        });
+    }
 }
